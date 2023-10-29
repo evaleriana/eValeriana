4a5d4700b86ea75dbd155d837c199c69dbdf1097
evaleriana <149321417+evaleriana@users.noreply.github.com>
2023-10-29 20:00:59 +0300

    Емельянова лаб 1

---
+++ Емельянова/task_3.py
@@ -0,0 +1,9 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+half = round(len(list_players) / 2)
+team1 = list_players [0:half]
+team2 = list_players [half:6]
+
+print(team1)
+print(team2)
